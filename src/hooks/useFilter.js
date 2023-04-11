a0224b4ff107dc9be0f8fc819a1e04cfd2c8062f
Michal Makowski <michal_makowski@external.mckinsey.com>
2023-04-11 21:54:39 +0200

    Removed comment

--- src/hooks/useFilter.js
+++ src/hooks/useFilter.js
@@ -1,7 +1,3 @@
 export const useFilter = (documents = [], condition = () => true, mapper = (item) => item) => {
     return documents.filter(condition).map(mapper);
 };
-
-// export const useSumReduce = (documents = [], reducer = (prev, curr) => prev + curr, initial = 0) => {
-//     return documents.reduce(reducer, initial);
-// }
